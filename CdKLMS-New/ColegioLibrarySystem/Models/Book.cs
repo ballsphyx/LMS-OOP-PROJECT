08ce90427932c708bc2ce859254abde6b827024c
Saikik2 <166888084+Saikik2@users.noreply.github.com>
2026-04-05 20:18:31 +0800

    jlkj

---
+++ CdKLMS-New/ColegioLibrarySystem/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColegioLibrarySystem.Models
+{
+    internal class Book
+    {
+        public int bookID { get; set; }
+        public string title { get; set; }
+        public string author { get; set; }
+        public string Catgory { get; set; }
+        public int totalCopies { get; set; }
+        //Add later on availableCopies//
+    }
+}
---
+++ CdKLMS-New/ColegioLibrarySystem/Models/BookCopy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColegioLibrarySystem.Models
+{
+    internal class BookCopy
+    {
+        public int copyID { get; set; }
+        public int bookID { get; set; }
+        public bool isAvailable { get; set; }
+    }
+}
